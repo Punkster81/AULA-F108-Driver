b3618fa412d741fc75628c49fc9a30742107d44a
Tanner Williams <tannerawilliams10@gmail.com>
2026-03-23 20:43:20 -0400

    v1.0.11 - added folder button in settings and fixed issues with soundboard

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import winreg
 import subprocess as _subprocess
 import multiprocessing
 
-VERSION = 'v1.0.10'
+VERSION = 'v1.0.11'
 GITHUB_REPO = 'Punkster81/AULA-F108-Driver'
 
 # ── Update system ─────────────────────────────────────────────────────────────
