021ac4356c57a7a67a8a8919fc71562a826baaf9
Tanner Williams <tannerawilliams10@gmail.com>
2026-03-25 21:35:08 -0400

    v1.0.14 - Smaller save files and moved functionality to the driver side to prevent throttling

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import winreg
 import subprocess as _subprocess
 import multiprocessing
 
-VERSION = 'v1.0.13'
+VERSION = 'v1.0.14'
 GITHUB_REPO = 'Punkster81/AULA-F108-Driver'
 
 # ── Update system ─────────────────────────────────────────────────────────────
